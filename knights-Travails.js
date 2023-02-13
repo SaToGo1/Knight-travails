00c8d929d4931e698758bf3b624c84841b29fd1d
SaToGo <stoledano97@gmail.com>
2023-02-13 23:50:28 +0100

    main file created and initial approach

---
+++ knights-Travails.js
@@ -0,0 +1,21 @@
+/*
+Board representation -> matrix.
+  
+  - 1 start of our knight.
+  - 2 target.
+Posibles Movments (?)
+  - 2 matrix one for posibles movments (?)
+*/ 
+
+let gameBoard = 
+[
+    [0, 0, 0, 0, 0, 0, 0, 0], // 1
+    [0, 0, 0, 0, 0, 0, 0, 0], // 2
+    [0, 0, 0, 0, 0, 0, 0, 0], // 3
+    [0, 0, 0, 0, 0, 0, 0, 0], // 4
+    [0, 0, 0, 0, 0, 0, 0, 0], // 5
+    [0, 0, 0, 0, 0, 0, 0, 0], // 6
+    [0, 0, 0, 0, 0, 0, 0, 0], // 7
+    [0, 0, 0, 0, 0, 0, 0, 0]  // 8
+]
+
