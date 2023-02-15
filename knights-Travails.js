b822c43bfddce5ccfa24ee36a313587a9dcaf292
SaToGo <stoledano97@gmail.com>
2023-02-15 20:49:45 +0100

    Add Possible Movments function
    add function possibleMovments()
    in knights-Travails.js file.

--- knights-Travails.js
+++ knights-Travails.js
@@ -16,3 +16,67 @@ let gameBoard =
     [0, 0, 0, 0, 0, 0, 0, 0]  // 8
 ]
 
+/**
+ * 
+ * @param {Coordinates} coordinates initial position of the knight.
+ * @returns array of the possible movments.
+ */
+const possibleMovments = (coordinates) => {
+
+
+    let x = coordinates.x;
+    let y = coordinates.y;
+
+    // Array of Movments [[x, y], [x, y], [x, y] ...]
+    let arrMov = [];
+
+    // Movment 1
+    if( ((x - 2) > 0) && ((y - 1) > 0) ){
+        let coor = new Coordinates(x-2,y-1);
+        arrMov.push(coor);
+    }
+
+    // Movment 2
+    else if( ((x - 1) > 0) && ((y - 2) > 0) ){
+        let coor = new Coordinates(x-1,y-2);
+        arrMov.push(coor);
+    }
+
+    // Movment 3
+    else if( ((x + 1) < 9) && ((y - 2) > 0) ){
+        let coor = new Coordinates(x+1,y-2);
+        arrMov.push(coor);
+    }
+
+    // Movment 4
+    else if( ((x + 2) < 9) && ((y - 1) > 0) ){
+        let coor = new Coordinates(x+2,y-1);
+        arrMov.push(coor);
+    }
+
+    // Movment 5
+    else if( ((x - 2) > 0) && ((y + 1) < 9) ){
+        let coor = new Coordinates(x-2,y+1);
+        arrMov.push(coor);
+    }
+
+    // Movment 6
+    else if( ((x - 1) > 0) && ((y + 2) < 9) ){
+        let coor = new Coordinates(x-1,y+2);
+        arrMov.push(coor);
+    }
+
+    // Movment 7
+    else if( ((x + 1) < 9) && ((y + 2) < 9) ){
+        let coor = new Coordinates(x+1,y+2);
+        arrMov.push(coor);
+    }
+
+    // Movment 8
+    else if( ((x + 2) < 9) && ((y + 1) < 9) ){
+        let coor = new Coordinates(x+2,y+1);
+        arrMov.push(coor);
+    }
+
+    return arrMov;
+}
